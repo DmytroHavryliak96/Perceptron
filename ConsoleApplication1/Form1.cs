a0d3c9f2e57bb03c48bd5498f166fd792e48c3da
DmytroHavryliak96 <dmytro.havryliak@gmail.com>
2016-09-14 21:25:08 +0300

    labPerceptron

---
+++ ConsoleApplication1/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace ConsoleApplication1
+{
+    public partial class Form1 : Form
+    {
+        private DataSet ds = new DataSet();
+        private DataTable dt = new DataTable();
+        private Perceptron p = new Perceptron();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connstring = "Server=127.0.0.1;Port=5432;User Id=postgres;Password=1postgres;Database=Labs;";
+                NpgsqlConnection conn = new NpgsqlConnection(connstring);
+                conn.Open();
+                string sql = "SELECT * FROM lab1";
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, conn);
+                ds.Reset();
+                da.Fill(ds);
+                dt = ds.Tables[0];
+                int i = 0;
+                foreach (DataRow row in dt.Rows) {
+                    for(int j = 0; j<3; j++)
+                    {
+                        int z;
+                        z = j+1;
+                        p.x[i, j] = Convert.ToInt32(row[Convert.ToString("x"+z)]);
+                    }
+                    i++;
+                }
+                dt.Reset();
+                conn.Close();
+            }
+            catch(Exception msg)
+            {
+                MessageBox.Show(msg.ToString());
+                throw;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dt.Columns.Add("w1");
+            dt.Columns.Add("w2");
+            dt.Columns.Add("w3");
+            dt.Columns.Add("teta");
+            dt.Columns.Add("rate");
+            dt.Columns.Add("x1");
+            dt.Columns.Add("x2");
+            dt.Columns.Add("x3");
+            dt.Columns.Add("a");
+            dt.Columns.Add("out");
+            dt.Columns.Add("true result");
+            dt.Columns.Add("delta1");
+            dt.Columns.Add("delta2");
+            dt.Columns.Add("delta3");
+            dt.Columns.Add("sigma");
+
+
+            bool finish;
+           
+            do
+            {
+                finish = false;
+                
+            for (int i = 0; i<8; i++)
+            {
+                p.activation(i);
+                p.study(i);
+
+                DataRow row = dt.NewRow();
+                row["w1"] = p.weights[0];
+                row["w2"] = p.weights[1];
+                row["w3"] = p.weights[2];
+                row["teta"] = Perceptron.teta;
+                row["rate"] = Perceptron.rate;
+                row["x1"] = p.x[i, 0];
+                row["x2"] = p.x[i, 1];
+                row["x3"] = p.x[i, 2];
+                row["a"] = p.a;
+                row["out"] = p.out1;
+                row["true result"] = p.true_result;
+                row["delta1"] = p.delta[0];
+                row["delta2"] = p.delta[1];
+                row["delta3"] = p.delta[2];
+                row["sigma"] = p.sigma[i];
+                dt.Rows.Add(row);
+            }
+                DataRow row1 = dt.NewRow();
+                dt.Rows.Add(row1);
+            dataGridView1.DataSource = dt;
+            for (int i = 0; i < p.sigma.Length; i++)
+            {
+               if (p.sigma[i] != 0)
+                 {
+                     finish = true;
+                     break;
+                 }
+                 else continue;
+             }
+           } while (finish);
+        }
+    }
+}
---
+++ ConsoleApplication1/Perceptron.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Perceptron
+    {
+        public int[,] x = new int[8, 3]; /*
+            /*{ 0, 0, 0 },
+            { 0, 0, 1 },
+            { 0, 1, 0 },
+            { 0, 1, 1 },
+            { 1, 0, 0 },
+            { 1, 0, 1 },
+            { 1, 1, 0 },
+            { 1, 1, 1 }
+        };*/
+        //private bool[,] x1 = new bool[8, 3];
+        public double[] weights = new double[3];
+        public const double rate = 0.1;
+        public const double teta = 0.3;
+        public int out1;
+        public int[] sigma = new int[8];
+        public double a;
+        public int true_result;
+        public double[]  delta = new double[3];
+
+        public Perceptron()
+        {
+            for(int i = 0; i<weights.Length; i++) {
+                Random rdn = new Random();
+                weights[i] = rdn.NextDouble() * (5.0) + 0.0;  
+            }
+        }
+
+        /*public void proceed()
+        {
+            for (int i = 0; i < 8; i++)
+            { 
+                activation(i);
+                study(i);
+                Output(i);
+            }
+            Console.WriteLine();
+        }*/
+
+        public void check()
+        {
+            bool finish;
+            //title();
+            do
+            {
+                finish = false;
+                //proceed();
+                for (int i = 0; i < sigma.Length; i++)
+                {
+                    if (sigma[i] != 0)
+                    {
+                        finish = true;
+                        break;
+                    }
+                    else continue;
+                }
+            } while (finish);
+        }
+
+        /*public void Output(int j)
+        {
+            for (int i = 0; i < weights.Length; i++)
+            {
+                Console.Write(Convert.ToString(weights[i]).PadRight(4));
+            }
+            Console.Write(Convert.ToString(teta).PadRight(4));
+
+            Console.Write(Convert.ToString(x[j, 0]).PadRight(4));
+            Console.Write(Convert.ToString(x[j, 1]).PadRight(4));
+            Console.Write(Convert.ToString(x[j, 2]).PadRight(4));
+
+            Console.Write(Convert.ToString(a).PadRight(4) + Convert.ToString(out1).PadRight(4)
+                + Convert.ToString(true_result).PadRight(4));
+
+           // Console.Write(Convert.ToString(rate * sigma[j]).PadRight(4));
+            /*Console.Write(Convert.ToString(delta2).PadRight(4));
+            Console.Write(Convert.ToString(delta3).PadRight(4));*/
+
+            /*for (int i = 0; i < weights.Length; i++)
+            {
+                Console.Write(Convert.ToString(sigma[j]*weights[i]).PadRight(4));
+            }
+            Console.Write(Convert.ToString(sigma[j] * teta).PadRight(4));
+            Console.Write(Convert.ToString(sigma[j]).PadRight(4));
+            Console.WriteLine();*/
+            /*for (int i = 0; i < 3; i++)
+            {
+                Console.Write(Convert.ToString(delta[i]).PadRight(4));
+            }
+
+            Console.Write(Convert.ToString(sigma[j]).PadRight(4));
+            Console.WriteLine();
+
+        }*/
+
+        /*public void title()
+        {
+            Console.WriteLine("w1".PadRight(4) + "w2".PadRight(4) + "w3".PadRight(4) +
+                "te".PadRight(4) + "x1".PadRight(4) + "x2".PadRight(4) + "x3".PadRight(4) +
+                    "a".PadRight(4) + "ou".PadRight(4) + "tr".PadRight(4) + "d1".PadRight(4) +
+                        "*d1".PadRight(4) + "*d3".PadRight(4) + "si".PadRight(4));
+        }
+        */
+        public void activation(int i)
+        {
+            true_result = Convert.ToInt32(!Convert.ToBoolean(x[i, 0]) && (!Convert.ToBoolean(x[i, 1]) || Convert.ToBoolean(x[i, 2])));
+            a = x[i, 0] * weights[0] + x[i, 1] * weights[1] + x[i, 2] * weights[2];
+            if (a > teta)
+            {
+                out1 = 1;
+            }
+            else
+            {
+                out1 = 0;
+            }
+            sigma[i] = true_result - out1;
+        }
+
+        public void study(int i)
+        {
+            if (out1 != true_result)
+            {
+
+                delta[0] = rate * sigma[i] * x[i, 0];
+                delta[1] = rate * sigma[i] * x[i, 1];
+                delta[2] = rate * sigma[i] * x[i, 2];
+
+                weights[0] += delta[0];
+                weights[1] += delta[1];
+                weights[2] += delta[2];
+            }
+
+        }
+    }
+}
+
+
